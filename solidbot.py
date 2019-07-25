56da403f2bfc8eedcac3f6cf41a1fbd3289f0925
Cameron Skaggs <cameronskaggscs@gmail.com>
2019-07-25 13:16:58 -0700

    File uploading, VC joining/leaving, and prototyped disconnect feature in the works.

--- solidbot.py
+++ solidbot.py
@@ -2,7 +2,6 @@ import math
 import re
 import random
 import discord
-import youtube_dl
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix = ';')
@@ -13,26 +12,34 @@ bot = commands.Bot(command_prefix = ';')
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     print('---------------')
-    # main
-    # channel = client.get_channel(517197086425350146)
-    # testburger
-    testburger = bot.get_channel(597934627050749963)
-    #mattChat = bot.get_channel(426613372025569280)
-    await testburger.send('WIP early build. Prefix commands with ;')
-    await testburger.send('Current commands are add, mult, sub, div, & pick.')
-    #await mattChat.send('WIP early build. Prefix commands with ;')
-    #await mattChat.send('Current commands are add/sum, multiply/mult, subtract/sub, divide/div pick/choose.')
 
 
-
+"""
+    Custom text modules
+"""
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
     elif message.content.lower().startswith('sharena'):
-        await message.channel.send('Hello!')
+        if any(x in message.content.lower() for x in ['the mission', 'above all', 'most important']):
+            with open('./assets/audio/VOICE_Alfonse_Prince_of_Askr_SKILL_1.wav', 'rb') as fp:
+                await message.channel.send(file=discord.File(fp, 'ABOVE_ALL.wav'))
+        elif re.search('^sharena,?\ destroy', message.content.lower()):
+            with open('./assets/images/sharena-destroy.png', 'rb') as fp:
+                await message.channel.send('With pleasure.', file=discord.File(fp, 'ladies_first.png'))
+    elif message.content.lower() == 'what' and message.author == 68568019751673856:
+        with open('./assets/audio/VOICE_Julia_Nagas_Blood_MAP_2.wav', 'rb') as fp:
+            await message.channel.send(file=discord.File(fp, 'what.wav'))
     await bot.process_commands(message)
 
+
+
+
+
+"""
+    Arithmetic modules
+"""
 @bot.command(aliases=['sum'])
 async def add(ctx, *args: str):
     """Adds multiple numbers together (decimal format only)."""
@@ -84,6 +91,12 @@ async def divide(ctx, left: float, right: float):
             await ctx.send('Usage: ;div dividend divisor')
             return
 
+
+
+
+"""
+    RNG modules
+"""
 @bot.command(aliases=['pick'])
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
@@ -94,6 +107,76 @@ async def choose(ctx, *choices: str):
         await ctx.send('Usage: ;choose thing a, thing b, ...')
         return
     await ctx.send(result)
+
+@bot.command(aliases=['random'])
+async def rng(ctx, left: int, right: int):
+    """Picks a random integer between two integers."""
+    try:
+        randNum = random.randint(left, right)
+    except:
+        await ctx.send('Usage: ;rng min max')
+        return
+    await ctx.send(randNum)
+
+
+
+
+
+"""
+    Voice modules
+"""
+@bot.command()
+async def join(ctx):
+    """Joins voice channel of current user."""
+    try: 
+        if not discord.opus.is_loaded():
+            discord.opus.load_opus('libopus.so.1')
+        userVoice = ctx.author.voice.channel
+        await userVoice.connect()
+    except Exception as e:
+        await ctx.send('Unable to join voice channel.')
+        print(e)
+        return
+
+@bot.command(aliases=['leave'])
+async def stop(ctx):
+    """Disconnects from current voice channel."""
+    try: 
+        if not discord.opus.is_loaded():
+            discord.opus.load_opus('libopus.so.1')
+        userVoice = ctx.author.voice.channel
+        for vc in bot.voice_clients:
+            if vc.guild == ctx.guild:
+                await vc.disconnect()
+    except Exception as e:
+        await ctx.send('Unable to leave voice channel.')
+        print(e)
+        return
+
+# @bot.command()
+# async def play(ctx):
+#     """Plays YouTube audio."""
+#     await ctx.send(';play command in progress, not yet working.')
+
+
+
+
+
+
+
+
+
+
+"""
+    Cleanup modules
+"""
+@bot.event
+async def on_disconnect():
+    print('{0.user} is disconnecting.'.format(bot))
+    await bot.close()
+
+
+
 
 
 bot.run('NjAwODc1MDU0NzMyOTM1MTY5.XS6J-g.Hi5TnhOJxJf72QLfGMIaDXagABY')
